a428da0afc04d9472d0d5a91cac1dae7993b21d0
nahum <nahum@DESKTOP-4C02UIV>
2022-12-12 23:48:55 -0600

    Commit Zujeily desde la pc de Nahun por problemas tecnicos

--- src/main/java/com/ProyectoFinal/controller/UsuarioController.java
+++ src/main/java/com/ProyectoFinal/controller/UsuarioController.java
@@ -2,47 +2,56 @@
 package com.ProyectoFinal.controller;
 
 
-import com.ProyectoFinal.DAO.UsuarioDao;
+
+import com.ProyectoFinal.Service.UsuarioService;
+import com.ProyectoFinal.domain.Usuario;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
 public class UsuarioController {
     
     @Autowired
-    private UsuarioDao usuarioDao;
+    private UsuarioService usuarioService;
     
     @GetMapping("/usuario/loginUsuario")
     public String Ingresar(Model model){
         log.info("Estamos usando arquitectura MVC");
         
-        var usuarios =usuarioDao.findAll();
-        model.addAttribute("usarios", usuarios);
+        var usuarios =usuarioService.getUsuarios();
+        model.addAttribute("usuarios", usuarios);
   
     return "/usuario/loginUsuario";
     } 
     
-    @GetMapping("/usuario/cuentaUsuario")
+    @GetMapping("/usuario/InfUsuario")
     public String PerfilUsuario(Model model){
         log.info("Estamos usando arquitectura MVC");
         
-        var usuarios =usuarioDao.findAll();
-        model.addAttribute("usarios", usuarios);
+        var usuarios =usuarioService.getUsuarios();
+        model.addAttribute("usuarios", usuarios);
         
-    return "/usuario/cuentaUsuario";
+    return "/usuario/InfUsuario";
     } 
     
-    @GetMapping("/usuario/formularioRegistroUsuario")
-    public String RegistrarOModificar(Model model){
+    @GetMapping("/usuario/Retroalimentacion")
+    public String RegistrarOModificar(Usuario usuario){
         log.info("Estamos usando arquitectura MVC");
-        
-        var usuarios =usuarioDao.findAll();
-        model.addAttribute("usarios", usuarios);
-    return "/usuario/formularioRegistroUsuario";
+      var usuarios =usuarioService.getUsuarios();
+    return "/usuario/Retroalimentacion";
+    } 
+ 
+    
+    @PostMapping("/usuario/guardarInfo")
+    public String guardarUsuario(Usuario usuario){
+        log.info("Estamos guardando");
+        usuarioService.save(usuario);
+    return "redirect:/usuario/Retroalimentacion";
     } 
     
 }
